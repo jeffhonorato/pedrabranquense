ac61afa65da4f9778425cf2ed222ba9c2b62f38f
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-04-23 11:06:46 -0300

    Ajustes Tabela Campeonato

---
+++ js/menu-fases.js
@@ -0,0 +1,19 @@
+const tabMenu = document.querySelectorAll(".js-tabela-menu a");
+const tabConteudo = document.querySelectorAll(".serie-a .fase__1");
+tabConteudo[0].classList.add("ativo")
+
+const activeTab = (index) => {
+
+    tabConteudo.forEach((conteudo) => {
+        conteudo.classList.remove("ativo");
+    });
+
+
+    tabConteudo[index].classList.add("ativo");
+}
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+        activeTab(index);
+    })
+})
