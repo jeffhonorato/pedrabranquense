ea820e982f73b6d90340705ab48af985b2421f2e
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2022-05-07 17:33:35 -0300

    Ajustes

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,1 +1,5 @@
-const menuBola = document.querySelector(".")
+const menuBola = document.querySelector("[data-menu-bola]");
+    menuBola.addEventListener("click", () => {
+        const nav = document.querySelector("[data-nav-menu]");
+        nav.classList.toggle("ativo")
+    })
